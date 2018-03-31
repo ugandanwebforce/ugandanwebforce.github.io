5c50ce598ef770e3fa579610fb842c706b0f9931
Alexander Korzun <korzun.sas@mail.ru>
2018-03-31 14:54:12 +0300

    Changed .gen/mod_dropdown_menu.py a bit

--- .gen/mod_dropdown_menu.py
+++ .gen/mod_dropdown_menu.py
@@ -2,11 +2,16 @@
 # -*- coding: utf-8 -*-
 
 def run(options):
-    ret = '''<ul class="dropdown-menu" role="menu">'''
+    ret = '''<li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+    Menu <span class="caret"></span>
+    </a>
+    <ul class="dropdown-menu" role="menu">'''
     for page in options['pages_list']:
         if page[0] == options['current_page']:
             ret += '''<li><span class="nav-current-page">{}</span></li>'''.format(page[1])
         else:
             ret += '''<li><a href="{}">{}</a></li>'''.format(page[2], page[1])
-    ret += '''</ul>'''
+    ret += '''</ul>
+    </li>'''
     return ret
